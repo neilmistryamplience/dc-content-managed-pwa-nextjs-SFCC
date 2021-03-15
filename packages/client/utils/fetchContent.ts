adf83703a5a2f6709a31a76f7705299a7bf9482f
Neil Mistry <neil@amplience.com>
2021-03-15 11:50:47 +0000

    hard coded localised content API calls

--- packages/client/utils/fetchContent.ts
+++ packages/client/utils/fetchContent.ts
@@ -4,14 +4,13 @@ import { NextPageContext } from 'next';
 // TODO: Add in locale support?
 
 export function fetchContent<T = any>(key: string, context?: NextPageContext): Promise<T> {
-    return fetch(`https://${context.query.contentApi || process.env.contentApi}/content/key/${key}?depth=all&format=inlined`)
+    return fetch(`https://${context.query.contentApi || process.env.contentApi}/content/key/${key}?depth=all&format=inlined&locale=en-GB,en-*,*`)
             .then(resp => resp.json())
             .then(body => body.content);
 }
 
 export function fetchContentById<T = any>(id: string, context?: NextPageContext): Promise<T> {
-    console.log("content ID: ", id)
-    return fetch(`https://${context.query.contentApi  || process.env.contentApi}/content/id/${id}?depth=all&format=inlined`)
+    return fetch(`https://${context.query.contentApi  || process.env.contentApi}/content/id/${id}?depth=all&format=inlined&locale=en-GB,en-*,*`)
             .then(resp => resp.json())
             .then(body => body.content);
 }
